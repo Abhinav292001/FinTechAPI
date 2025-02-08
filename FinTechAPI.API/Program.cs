12e4a5ce99c2a2fe58e1d62c47bdb7b406af88ff
Sai Abhinav Sirimamilla <navabhi29.s2@gmail.com>
2025-02-08 23:47:56 +0530

    Project Basic Setup

---
+++ FinTechAPI.API/Program.cs
@@ -0,0 +1,37 @@
+using FinTechAPI.Application.Interfaces;
+using FinTechAPI.Application.Services;
+using FinTechAPI.Infrastructure.Persistence;
+using FinTechAPI.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+builder.Services.AddScoped<TransactionService>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ FinTechAPI.Application/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,19 @@
+﻿using FinTechAPI.Domain.Entities;
+
+namespace FinTechAPI.Application.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        /// <summary>
+        /// This method helps to get the list of Transactions.
+        /// </summary>
+        /// <returns>List of Transactions</returns>
+        Task<List<Transaction>> GetTransactionsAsync();
+        /// <summary>
+        /// This method helps to add a new transaction.
+        /// </summary>
+        /// <param name="transaction">transaction</param>
+        /// <returns>Transaction</returns>
+        Task<Transaction> AddTransactionAsync(Transaction transaction);
+    }
+}
